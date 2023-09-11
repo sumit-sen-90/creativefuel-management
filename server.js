6912d3172f1fb640734f6679dae0556ea7d46641
Sumit <sumit223312@gmail.com>
2023-09-11 22:27:37 +0530

    update Server

--- server.js
+++ server.js
@@ -9,6 +9,10 @@ const PORT = process.env.PORT || 3000;
 app.use(express.json());
 app.use(cors());
 
+app.use(express.static(path.join(__dirname,'./client/build')))
+app.get("*",function (req,res){
+    res.sendFile(path.join(__dirname,"./client/build/index.html"))
+})
 
 //User routes
 const userRouter = require("./routes/testRoutes");
